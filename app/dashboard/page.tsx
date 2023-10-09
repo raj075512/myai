abeb3df8baff3d9205b6d8a57b710265d6f248ec
raj075512 <raj075512>
2023-10-09 17:18:32 +0530

    adding the neon data base  as the serverless postgres

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
-interface Props {
-    
-}
+interface Props {}
 
-const page = (props: Props) => {
-    return (
-        <div>
-            
+const dashboard = (props: Props) => {
+  return (
+    <div>
+      <div className="min-h-screen">
+        <div className="p-10 mx-auto max-w-7xl">
+          <div className=" flex justify-center color-black p-5 font-medium text-3xl ">
+            <h1 className="mx-3">DashBoard</h1>
+            <UserButton />
+          </div>
+          <div className=" flex items-center justify-between">
+            <Button className="bg-green-500 " size="sm">
+              go back
+            </Button>
+            <Button variant="outline">create new</Button>
+          </div>
         </div>
-    )
-}
+    
+        <Separator />
+ 
+        <div className=" text-center ">
+            <h2> you  have no text  yet ..</h2>
+          
+        </div>
 
-export default page
+      </div>
+    </div>
+  );
+};
+
+export default dashboard;
