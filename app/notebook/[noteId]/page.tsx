c7fe26de06e857152775bf5b774cfae1ce5b6bf8
raj075512 <raj075512>
2023-10-14 18:10:58 +0530

    adding the text-editor as the tiptap

--- app/notebook/[noteId]/page.tsx
+++ app/notebook/[noteId]/page.tsx
@@ -5,9 +5,11 @@ import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { clerk } from "@/lib/clerk-server";
 import { $notes } from '@/lib/db/schema';
-
+import  Link  from "next/link"
  import { db } from '@/lib/db';
 import { and ,eq } from 'drizzle-orm';
+import { Button } from '@/components/ui/button';
+
 
 type Props = {
     params: {
@@ -35,9 +37,31 @@ type Props = {
     const note = notes[0];
   
     return (
-        <div>
-             {noteId}
+      <div className="min-h-screen grainy p-8">
+      <div className="max-w-4xl mx-auto">
+        <div className="border shadow-xl border-stone-200 rounded-lg p-4 flex items-center">
+          <Link href="/dashboard">
+            <Button className="bg-green-600" size="sm">
+              Back
+            </Button>
+          </Link>
+          <div className="w-3"></div>
+          <span className="font-semibold">
+            {user.firstName} {user.lastName}
+          </span>
+          <span className="inline-block mx-1">/</span>
+          <span className="text-stone-500 font-semibold">{note.name}</span>
+          <div className="ml-auto">
+            <DeleteButton noteId={note.id} />
+          </div>
         </div>
+
+        <div className="h-4"></div>
+        <div className="border-stone-200 shadow-xl border rounded-lg px-16 py-8 w-full">
+          <TipTapEditor note={note} />
+        </div>
+      </div>
+    </div>
     )
 }
 
---
+++ components/TipTapMenuEditor.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Editor } from '@tiptap/react'
+
+type Props= {
+    editor:editor ;
+}
+
+const TipTapMenuBar = ( {editor}: Props) => {
+    return (
+        <div>
+              
+        </div>
+    )
+}
+
+export default TipTapMenuBar
